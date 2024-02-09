6c38bd00a8745d858763b8bb5056cc9cfb8a59bf
Kevin Dayno <57159543+kdayno@users.noreply.github.com>
2024-02-09 03:50:11 +0000

    Add changes for module 2.2.7

---
+++ 02-workflow-orchestration/terraform/variables.tf
@@ -0,0 +1,73 @@
+variable "app_name" {
+  type        = string
+  description = "Application Name"
+  default     = "mage-data-kd"
+}
+
+variable "container_cpu" {
+  description = "Container cpu"
+  default     = "2000m"
+}
+
+variable "container_memory" {
+  description = "Container memory"
+  default     = "2G"
+}
+
+variable "project_id" {
+  type        = string
+  description = "The name of the project"
+  default     = "data-eng-zoomcamp-2024-412913"
+}
+
+variable "region" {
+  type        = string
+  description = "The default compute region"
+  default     = "northamerica-northeast2"
+}
+# us-west2
+# northamerica-northeast2
+
+variable "zone" {
+  type        = string
+  description = "The default compute zone"
+  default     = "northamerica-northeast2-a"
+}
+# us-west2-a
+# northamerica-northeast2-a
+
+variable "repository" {
+  type        = string
+  description = "The name of the Artifact Registry repository to be created"
+  default     = "mage-data-kd"
+}
+
+variable "database_user" {
+  type        = string
+  description = "The username of the Postgres database."
+  default     = "mageuser"
+}
+
+variable "database_password" {
+  type        = string
+  description = "The password of the Postgres database."
+  sensitive   = true
+}
+
+variable "docker_image" {
+  type        = string
+  description = "The docker image to deploy to Cloud Run."
+  default     = "mageai/mageai:latest"
+}
+
+variable "domain" {
+  description = "Domain name to run the load balancer on. Used if `ssl` is `true`."
+  type        = string
+  default     = ""
+}
+
+variable "ssl" {
+  description = "Run load balancer on HTTPS and provision managed certificate with provided `domain`."
+  type        = bool
+  default     = false
+}
